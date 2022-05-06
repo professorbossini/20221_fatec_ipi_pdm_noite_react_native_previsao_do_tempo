30aa918bb565bf008f7d02f8ce4b86f3b9f4aa6b
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-06 20:59:31 -0300

    Variável de estado para a coleção de previsões.

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import{
 
 export default function App() {
   const [cidade, setCidade] = useState('')
+  const [previsoes, setPrevisoes] = useState([])
   const capturarCidade = (cidadeDigitada) => {
     setCidade(cidadeDigitada)
   }
@@ -35,7 +36,14 @@ export default function App() {
 
       </View>
       {/* exibição das previsões */}
-      <FlatList />
+      <FlatList 
+        data={previsoes}
+        renderItem={
+          p => (
+            <Text>{JSON.stringify(p)}</Text>
+          )
+        }
+      />
 
       
     </View>
